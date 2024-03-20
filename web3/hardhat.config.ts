60d8a4b65406c4698289d1d016033f497e8df0b1
FahimDev <fahim.arif0373@outlook.com>
2024-03-20 14:18:40 +0600

    feat: fincube-1 Adds Storage Layout
    Command:
    - npm i hardhat-storage-layout
    - npx hardhat compile
    - npx hardhat check
    
    Make sure to import 'hardhat-storage-layout';
    
    Ref:
    - https://www.npmjs.com/package/hardhat-storage-layout

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-storage-layout';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
