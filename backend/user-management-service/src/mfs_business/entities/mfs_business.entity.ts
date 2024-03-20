a4106bd7faa81ab40f61dc79795386b9908cd710
mashiat0808 <mashiatamin08@gmail.com>
2024-03-20 14:14:43 +0600

    feat: fincube-5 postgresql connection init

--- backend/user-management-service/src/mfs_business/entities/mfs_business.entity.ts
+++ backend/user-management-service/src/mfs_business/entities/mfs_business.entity.ts
@@ -1,1 +1,34 @@
-export class MfsBusiness {}
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+//import { DaoEntity } from './dao/entities/dao.entity';
+import { ExchangeUserEntity } from 'src/exchange_user/entities/exchange_user.entity';
+
+@Entity('mfs_business')
+export class MfsBusinessEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  org_email: string;
+
+  @Column({ type: 'varchar' })
+  wallet_address: string;
+
+  @Column({ type: 'varchar' })
+  native_currency: string;
+
+  @Column({ type: 'varchar' })
+  certificate: string;
+
+  //@ManyToOne(() => DaoEntity, (dao) => dao.businesses)
+  // @JoinColumn({ name: 'dao_id' })
+  //dao: DaoEntity;
+
+  @OneToMany(
+    () => ExchangeUserEntity,
+    (exchangeUser) => exchangeUser.mfsBusiness,
+  )
+  exchangeUsers: ExchangeUserEntity[];
+}
