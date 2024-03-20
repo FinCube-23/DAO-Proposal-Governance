4eca8d86c06535f31677813a3cd7c90acca2ae38
FahimDev <fahim.arif0373@outlook.com>
2024-03-20 14:24:14 +0600

    feat: fincube-1 Adds HardHat Gas Reporter
    Command:
    - npm i hardhat-gas-reporter
    - npx hardhat compile
    - npx hardhat test
    Ref:
    - https://www.npmjs.com/package/hardhat-gas-reporter

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'hardhat-storage-layout';
+import "hardhat-gas-reporter"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
