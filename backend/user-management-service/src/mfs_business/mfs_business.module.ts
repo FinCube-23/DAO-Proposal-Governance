fdef5108d722ff8dff5bb9e8fa16e39f2bc914a8
mashiat0808 <mashiatamin08@gmail.com>
2024-03-20 14:48:35 +0600

    feat:fincube-5 posgres server setup with docker image completed

--- backend/user-management-service/src/mfs_business/mfs_business.module.ts
+++ backend/user-management-service/src/mfs_business/mfs_business.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MfsBusinessService } from './mfs_business.service';
 import { MfsBusinessController } from './mfs_business.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MfsBusinessEntity } from 'src/mfs_business/entities/mfs_business.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MfsBusinessEntity])],
   controllers: [MfsBusinessController],
   providers: [MfsBusinessService],
 })
