6a544ce5c6816495344a0d528baf295ea434053e
FahimDev <fahim.arif0373@outlook.com>
2024-03-27 18:11:17 +0600

    feat: fincube-16 Configures Database Connection
    Install Type ORM
    - npm install @nestjs/typeorm typeorm pg
    Create a Database module for repeated use
    - nest g resource database
    Make sure you have added use env file with custom names at app.module.ts
    -
        ConfigModule.forRoot({
          isGlobal: true,
          envFilePath: '.env.local',
        }),

--- backend/audit-trail-service/src/app.module.ts
+++ backend/audit-trail-service/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.local',
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
