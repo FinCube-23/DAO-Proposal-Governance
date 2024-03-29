3a0d0ce3bfe1bf13617aa240a19629ebf5c28716
mashiat0808 <mashiatamin08@gmail.com>
2024-03-29 13:36:41 +0600

    fixed indentation issues

--- web3/contracts/fincubeDAO.sol
+++ web3/contracts/fincubeDAO.sol
@@ -7,35 +7,35 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  */
 
 contract FinCubeDAO is Ownable {
- 
-   event MemberRegistered(address indexed _newMember, string _memberURI);
-   event ProposalCreated(
-       uint256 indexed proposalId,
-       ProposalType indexed proposalType,
-       bytes data
+    event MemberRegistered(address indexed _newMember, string _memberURI);
+    event ProposalCreated(
+        uint256 indexed proposalId,
+        ProposalType indexed proposalType,
+        bytes data
+    );
 
-   );
-
-   event MemberApproved(address indexed member);
-   event ProposalExecuted(uint256 indexed proposalId);
-   uint256 private memberCount;
-   uint256 private proposalCount;
-       /**
+    event MemberApproved(address indexed member);
+    event ProposalExecuted(uint256 indexed proposalId);
+    uint256 private memberCount;
+    uint256 private proposalCount;
+    /**
      * @dev Represents a member of the DAO.
      * @param memberURI The URI that identifies the member.
      * @param status Whether the member is approved or not.
      */
-   struct Member {
-       string memberURI;
-       bool status;
-   }
- /**
+    struct Member {
+        string memberURI;
+        bool status;
+    }
+    /**
      * @dev Represents the different types of proposals that can be created.
      */
-   enum ProposalType {
-    NewMemberProposal, TokenAddressProposal, RoyaltyProposal
-   }
-   
+    enum ProposalType {
+        NewMemberProposal,
+        TokenAddressProposal,
+        RoyaltyProposal
+    }
+
     /**
      * @dev Represents a proposal within the DAO.
      * @param proposer The address of the member who created the proposal.
@@ -58,99 +58,99 @@ contract FinCubeDAO is Ownable {
         uint256 novotes;
     }
 
-        /**
+    /**
      * @dev Stores the vote information for a proposal.
      * @param isYesVote Mapping of addresses to whether they voted "yes" or not.
      * @param isNoVote Mapping of addresses to whether they voted "no" or not.
      */
-   struct ProposalVotes {
-       mapping(address => bool) isYesVote;
-       mapping(address => bool) isNoVote;
-   }
+    struct ProposalVotes {
+        mapping(address => bool) isYesVote;
+        mapping(address => bool) isNoVote;
+    }
     mapping(address => Member) private members;
     mapping(uint256 => Proposal) private proposals;
     mapping(uint256 => ProposalVotes) private proposalVotes;
     mapping(uint256 => ProposalType) private proposalType;
     address public tokenAddress;
- 
-     /**
+
+    /**
      * @notice Initializes the contract with the owner as the first member.
      * @param _ownerURI The URI that identifies the owner member.
      */
-   constructor(string memory _ownerURI) Ownable(msg.sender) {
-       Member memory member;
-       member.memberURI = _ownerURI;
-       member.status = true;
-       members[msg.sender] = member;
-       memberCount = 1;
-   }
+    constructor(string memory _ownerURI) Ownable(msg.sender) {
+        Member memory member;
+        member.memberURI = _ownerURI;
+        member.status = true;
+        members[msg.sender] = member;
+        memberCount = 1;
+    }
 
-       /**
+    /**
      * @notice Returns the voting delay period in seconds.
      * @dev This function is marked as `pure` because it does not read or modify the contract's state.
      * @return The voting delay period in seconds (currently 5 seconds).
      */
-   function votingDelay() public pure returns (uint256) {
-       return 5; // 5 seconds
-   }
+    function votingDelay() public pure returns (uint256) {
+        return 5; // 5 seconds
+    }
 
-       /**
+    /**
      * @notice Returns the voting period duration in seconds.
      * @dev This function is marked as `pure` because it does not read or modify the contract's state.
      * @return The voting period duration in seconds (currently 60 seconds).
      */
-   function votingPeriod() public pure returns (uint256) {
-       return 60; // 1 minute
-   }
+    function votingPeriod() public pure returns (uint256) {
+        return 60; // 1 minute
+    }
 
-       /**
+    /**
      * @notice Returns the proposal threshold, which is the minimum number of "yes" votes required for a proposal to be executed.
      * @dev The proposal threshold is calculated as (memberCount + 1) / 2.
      * @return The proposal threshold.
      */
-   function proposalThreshold() public view returns (uint256) {
-       return (memberCount +1)/ 2;
-   }
-       /**
+    function proposalThreshold() public view returns (uint256) {
+        return (memberCount + 1) / 2;
+    }
+    /**
      * @notice Modifier to ensure that the provided address is not already a member.
      * @param _address The address to be checked.
      */
-   modifier isNotExistingMember(address _address) {
-       require(
-           !(bytes(members[_address].memberURI).length > 0),
-           "Already a member"
-       );
-       _;
-   }
-       /**
+    modifier isNotExistingMember(address _address) {
+        require(
+            !(bytes(members[_address].memberURI).length > 0),
+            "Already a member"
+        );
+        _;
+    }
+    /**
      * @notice Modifier to ensure that the provided address is a valid and approved member.
      * @param _address The address to be checked.
      */
-   modifier onlyMember(address _address) {
-       require(
-           (bytes(members[_address].memberURI).length > 0),
-           "Not a member"
-       );
-       require(members[_address].status == true, "Member not approved");
-       _;
-   }
- 
-   /**
-* @notice Register a new member.
-* @dev This function can only be called by non-existing members.
-* @param _newMember The address of the new member to be registered.
-* @param _memberURI The URI of the new member.
-*/
-   function registerMember(
-       address _newMember,
-       string memory _memberURI
-   ) external isNotExistingMember(_newMember) {
-       Member memory member;
-       member.memberURI = _memberURI;
-       member.status = false;
-       members[_newMember] = member;
-       emit MemberRegistered(_newMember, _memberURI);
-   }
+    modifier onlyMember(address _address) {
+        require(
+            (bytes(members[_address].memberURI).length > 0),
+            "Not a member"
+        );
+        require(members[_address].status == true, "Member not approved");
+        _;
+    }
+
+    /**
+     * @notice Register a new member.
+     * @dev This function can only be called by non-existing members.
+     * @param _newMember The address of the new member to be registered.
+     * @param _memberURI The URI of the new member.
+     */
+    function registerMember(
+        address _newMember,
+        string memory _memberURI
+    ) external isNotExistingMember(_newMember) {
+        Member memory member;
+        member.memberURI = _memberURI;
+        member.status = false;
+        members[_newMember] = member;
+        emit MemberRegistered(_newMember, _memberURI);
+    }
     /**
      * @notice Creates a new proposal for approving a new member.
      * @dev This function can only be called by an existing member.
@@ -173,8 +173,12 @@ contract FinCubeDAO is Ownable {
             yesvotes: 0,
             novotes: 0
         });
-        proposalType[proposalCount]= ProposalType.NewMemberProposal;
-        emit ProposalCreated(proposalCount, ProposalType.NewMemberProposal, _data);
+        proposalType[proposalCount] = ProposalType.NewMemberProposal;
+        emit ProposalCreated(
+            proposalCount,
+            ProposalType.NewMemberProposal,
+            _data
+        );
         proposalCount++;
     }
     /**
@@ -182,25 +186,31 @@ contract FinCubeDAO is Ownable {
      * @dev This function can only be called by an existing member.
      * @param _tokenAddress The address of the token to be set.
      */
-   function newTokenAddressProposal(address _tokenAddress) external onlyMember(msg.sender) {
-    uint48 currentTime = uint48(block.timestamp);
-    uint48 start = currentTime + uint48(votingDelay());
-    uint48 end = start + uint48(votingPeriod());
-    bytes memory _data = toBytes(_tokenAddress);
-    proposals[proposalCount] = Proposal({
-        proposer: msg.sender,
-        voteStart: start,
-        voteDuration: end,
-        executed: false,
-        canceled: false,
-        data: _data,
-        yesvotes: 0,
-        novotes: 0
-    });
-    proposalType[proposalCount]= ProposalType.TokenAddressProposal;
-    emit ProposalCreated(proposalCount,ProposalType.TokenAddressProposal, _data);
-    proposalCount++;
-}
+    function newTokenAddressProposal(
+        address _tokenAddress
+    ) external onlyMember(msg.sender) {
+        uint48 currentTime = uint48(block.timestamp);
+        uint48 start = currentTime + uint48(votingDelay());
+        uint48 end = start + uint48(votingPeriod());
+        bytes memory _data = toBytes(_tokenAddress);
+        proposals[proposalCount] = Proposal({
+            proposer: msg.sender,
+            voteStart: start,
+            voteDuration: end,
+            executed: false,
+            canceled: false,
+            data: _data,
+            yesvotes: 0,
+            novotes: 0
+        });
+        proposalType[proposalCount] = ProposalType.TokenAddressProposal;
+        emit ProposalCreated(
+            proposalCount,
+            ProposalType.TokenAddressProposal,
+            _data
+        );
+        proposalCount++;
+    }
 
     /**
      * @notice Casts a vote for a proposal.
@@ -208,13 +218,22 @@ contract FinCubeDAO is Ownable {
      * @param _proposalId The ID of the proposal to vote for.
      * @param _isYesVote Whether the vote is a "yes" vote (true) or a "no" vote (false).
      */
-function castVote(uint256 _proposalId, bool _isYesVote) external onlyMember(msg.sender) {
-    
+    function castVote(
+        uint256 _proposalId,
+        bool _isYesVote
+    ) external onlyMember(msg.sender) {
         ProposalVotes storage votes = proposalVotes[_proposalId];
         Proposal storage proposal = proposals[_proposalId];
 
-        require(block.timestamp >= proposal.voteStart && block.timestamp <= proposal.voteDuration, "Voting is not allowed at this time");
-        require(!votes.isYesVote[msg.sender] && !votes.isNoVote[msg.sender], "Already voted for this proposal");
+        require(
+            block.timestamp >= proposal.voteStart &&
+                block.timestamp <= proposal.voteDuration,
+            "Voting is not allowed at this time"
+        );
+        require(
+            !votes.isYesVote[msg.sender] && !votes.isNoVote[msg.sender],
+            "Already voted for this proposal"
+        );
 
         if (_isYesVote) {
             proposal.yesvotes++;
@@ -223,7 +242,7 @@ function castVote(uint256 _proposalId, bool _isYesVote) external onlyMember(msg.
             proposal.novotes++;
             votes.isNoVote[msg.sender] = true;
         }
-    } 
+    }
 
     /**
      * @notice Executes a proposal if it meets the requirements.
@@ -231,68 +250,64 @@ function castVote(uint256 _proposalId, bool _isYesVote) external onlyMember(msg.
      * @param proposalId The ID of the proposal to be executed.
      */
     function executeProposal(uint256 proposalId) public {
-        Proposal memory proposal =  proposals[proposalId];
-         require(
-           !(block.timestamp >= proposal.voteStart &&
-               block.timestamp <= proposal.voteDuration),
-           "Voting still going on"
-       );
-       require(
-           !proposal.executed && !proposal.canceled,
-           "Proposal already executed or canceled"
-       );
-       require(
-           proposal.yesvotes >= proposalThreshold(),
-           "Proposal doesn't have majority vote"
-       );
-       if(proposalType[proposalId]==ProposalType.NewMemberProposal) {
-       
+        Proposal memory proposal = proposals[proposalId];
+        require(
+            !(block.timestamp >= proposal.voteStart &&
+                block.timestamp <= proposal.voteDuration),
+            "Voting still going on"
+        );
+        require(
+            !proposal.executed && !proposal.canceled,
+            "Proposal already executed or canceled"
+        );
+        require(
+            proposal.yesvotes >= proposalThreshold(),
+            "Proposal doesn't have majority vote"
+        );
+        if (proposalType[proposalId] == ProposalType.NewMemberProposal) {
             approveMember(bytesToAddress(proposal.data));
             proposal.executed = true;
             emit ProposalExecuted(proposalId);
-       }
-       else if(proposalType[proposalId]==ProposalType.TokenAddressProposal){
-         setTokenAddress(bytesToAddress(proposal.data));
+        } else if (
+            proposalType[proposalId] == ProposalType.TokenAddressProposal
+        ) {
+            setTokenAddress(bytesToAddress(proposal.data));
             proposal.executed = true;
             emit ProposalExecuted(proposalId);
-       }
+        }
     }
- 
-   
-      /**
+
+    /**
      * @notice Returns the count of ongoing proposals.
      * @dev This function iterates through the proposals mapping and counts the proposals that have not been executed or canceled and are within the voting period.
      * @return The count of ongoing proposals.
      */
 
-
-
-
-     function getOngoingProposalsCount() public view returns (uint256) {
-    uint256 ongoingCount = 0;
-    for (uint256 i = 0; i < proposalCount; i++) {
-        if (
-            !proposals[i].executed &&
-            !proposals[i].canceled &&
-            block.timestamp >= proposals[i].voteStart &&
-            block.timestamp <= proposals[i].voteDuration
-        ) {
-            ongoingCount++;
+    function getOngoingProposalsCount() public view returns (uint256) {
+        uint256 ongoingCount = 0;
+        for (uint256 i = 0; i < proposalCount; i++) {
+            if (
+                !proposals[i].executed &&
+                !proposals[i].canceled &&
+                block.timestamp >= proposals[i].voteStart &&
+                block.timestamp <= proposals[i].voteDuration
+            ) {
+                ongoingCount++;
+            }
         }
-    }
-    return ongoingCount;
+        return ongoingCount;
     }
 
-        /**
+    /**
      * @notice Returns an array of ongoing proposals.
      * @dev This function first calls `getOngoingProposalsCount` to get the count of ongoing proposals, then iterates through the proposals mapping and populates an array with the ongoing proposals.
      * @return An array of ongoing proposals.
      */
- 
+
     function getOngoingProposals() public view returns (Proposal[] memory) {
         uint256 ongoingCount = getOngoingProposalsCount();
         Proposal[] memory ongoingProposals = new Proposal[](ongoingCount);
-    
+
         uint256 index = 0;
         for (uint256 i = 0; i < proposalCount; i++) {
             if (
@@ -304,9 +319,9 @@ function castVote(uint256 _proposalId, bool _isYesVote) external onlyMember(msg.
                 ongoingProposals[index] = proposals[i];
                 index++;
             }
-         }
- 
-    return ongoingProposals;
+        }
+
+        return ongoingProposals;
     }
     /**
      * @notice Sets the token address.
@@ -317,29 +332,30 @@ function castVote(uint256 _proposalId, bool _isYesVote) external onlyMember(msg.
     function setTokenAddress(address _tokenAddress) private {
         tokenAddress = _tokenAddress;
     }
-   /**
+    /**
      * @notice Approves a new member.
      * @dev This function is marked as `private` because it should only be called internally by the `executeProposal` function.
      * @param newMember The address of the new member to be approved.
      */
-     function approveMember(address newMember) private  {
-       require(!members[newMember].status, "Member already approved");
-       members[newMember].status = true;
-       memberCount++;
-       emit MemberApproved(newMember);
-   }
+    function approveMember(address newMember) private {
+        require(!members[newMember].status, "Member already approved");
+        members[newMember].status = true;
+        memberCount++;
+        emit MemberApproved(newMember);
+    }
 
-
-   /**
+    /**
      * @notice Converts a byte array to an address.
      * @dev This function is marked as `private` and `pure` because it does not read or modify the contract's state and is only used internally.
      * @param bys The byte array to be converted.
      * @return addr The address converted from the byte array.
      */
-    function bytesToAddress(bytes memory bys) private pure returns (address addr) {
+    function bytesToAddress(
+        bytes memory bys
+    ) private pure returns (address addr) {
         assembly {
-            addr := mload(add(bys,20))
-        } 
+            addr := mload(add(bys, 20))
+        }
     }
 
     /**
@@ -351,18 +367,17 @@ function castVote(uint256 _proposalId, bool _isYesVote) external onlyMember(msg.
     function toBytes(address a) private pure returns (bytes memory) {
         return abi.encodePacked(a);
     }
- 
-   //@warning: function only defined for testing purposes
+
+    //@warning: function only defined for testing purposes
     /**
      * @notice Checks if a member is approved.
      * @dev This function can only be called by an existing member.
      * @param _member The address of the member to be checked.
      * @return A boolean indicating whether the member is approved or not.
      */
-   function checkIsMemberApproved(
-       address _member
-   ) public view onlyMember(msg.sender) returns (bool) {
-       return members[_member].status;
-   }
- 
+    function checkIsMemberApproved(
+        address _member
+    ) public view onlyMember(msg.sender) returns (bool) {
+        return members[_member].status;
+    }
 }
