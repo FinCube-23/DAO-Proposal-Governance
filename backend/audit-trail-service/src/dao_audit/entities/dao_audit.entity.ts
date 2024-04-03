f97ac19949dca4b82af458ce935e950a09af573b
FahimDev <fahim.arif0373@outlook.com>
2024-04-03 17:51:55 +0600

    feat: fincube 15 Designs DB Table Schema

--- backend/audit-trail-service/src/dao_audit/entities/dao_audit.entity.ts
+++ backend/audit-trail-service/src/dao_audit/entities/dao_audit.entity.ts
@@ -1,1 +1,20 @@
-export class DaoAudit {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn  } from 'typeorm';
+
+@Entity('dao_transaction')
+export class DaoAudit {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ type: 'varchar', unique: true })
+    trx_hash: string;
+
+    @Column({ type: 'varchar' })
+    trx_sender: string;
+  
+    @Column({ type: 'integer', nullable: true })
+    trx_status: number;
+
+    @CreateDateColumn({ name: 'created_at' }) 'created_at': Date;
+
+    @UpdateDateColumn({ name: 'updated_at' }) 'updated_at': Date;
+}
