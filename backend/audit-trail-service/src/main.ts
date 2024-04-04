bf8e3c6629c9f256439e4cdad20f1368f6fc4b55
mashiat0808 <mashiatamin08@gmail.com>
2024-04-04 17:10:01 +0600

    feat: fincube-41 audit trail swagger implemented

--- backend/audit-trail-service/src/main.ts
+++ backend/audit-trail-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
