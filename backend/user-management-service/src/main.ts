f1209b2320f3b62cbf8b016b264f8d6f170851a9
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2024-04-04 14:04:23 +0600

    feat:fincube-10 Swagger API setup for User management service

--- backend/user-management-service/src/main.ts
+++ backend/user-management-service/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('User Management Service API')
+    .setDescription('User management service provides registration and login for users')
+    .setVersion('1.0')
+    .addTag('user-management')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'],
+  });
   await app.listen(3000);
 }
 bootstrap();
