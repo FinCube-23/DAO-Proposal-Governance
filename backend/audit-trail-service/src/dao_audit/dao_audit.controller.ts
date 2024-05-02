99edb72c40530f5365219dc47fd2c81d93d34d4a
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2024-05-02 12:44:31 +0600

    hotfix: Swagger API setup in Audit Trial Services

--- backend/audit-trail-service/src/dao_audit/dao_audit.controller.ts
+++ backend/audit-trail-service/src/dao_audit/dao_audit.controller.ts
@@ -1,34 +1,31 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { DaoAuditService } from './dao_audit.service';
-import { CreateDaoAuditDto } from './dto/create-dao_audit.dto';
-import { UpdateDaoAuditDto } from './dto/update-dao_audit.dto';
-
+import { DaoAudit } from "./entities/dao_audit.entity";
+import { ApiBody, ApiResponse } from '@nestjs/swagger';
 @Controller('dao-audit')
 export class DaoAuditController {
-  constructor(private readonly daoAuditService: DaoAuditService) {}
+  constructor(private readonly daoAuditService: DaoAuditService) { }
 
   @Post()
-  create(@Body() createDaoAuditDto: CreateDaoAuditDto) {
-    return this.daoAuditService.create(createDaoAuditDto);
+  @ApiBody({ type: DaoAudit })
+  @ApiResponse({ status: 200, description: 'The record has been successfully created.', type: DaoAudit })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  create(@Body() daoAudit: DaoAudit) {
+    return this.daoAuditService.create(daoAudit);
   }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'The record has been successfully updated.', type: DaoAudit })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   findAll() {
     return this.daoAuditService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'The record has been successfully updated.', type: DaoAudit })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   findOne(@Param('id') id: string) {
     return this.daoAuditService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDaoAuditDto: UpdateDaoAuditDto) {
-    return this.daoAuditService.update(+id, updateDaoAuditDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.daoAuditService.remove(+id);
-  }
 }
--- backend/audit-trail-service/src/dao_audit/dto/update-dao_audit.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDaoAuditDto } from './create-dao_audit.dto';
-
-export class UpdateDaoAuditDto extends PartialType(CreateDaoAuditDto) {}
