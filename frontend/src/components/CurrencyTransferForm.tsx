fa40feec0c93c14d21b1a47d2ab9a4ff7fe2dcda
Antonin Islam <antonin.islam@brainstation-23.com>
2024-05-13 18:18:27 +0600

    adds currency transfer ui

---
+++ frontend/src/components/CurrencyTransferForm.tsx
@@ -0,0 +1,6 @@
+
+export default function CurrencyTransferForm() {
+  return (
+    <div>CurrencyTransferForm</div>
+  )
+}
---
+++ frontend/src/pages/tests.tsx
@@ -0,0 +1,11 @@
+import CurrencyTranster from "@components/CurrencyTranster";
+
+export default function Tests() {
+  return (
+    <div className="container mx-auto mt-20">
+      <div className="flex flex-col gap-5">
+        <CurrencyTranster />
+      </div>
+    </div>
+  )
+}
