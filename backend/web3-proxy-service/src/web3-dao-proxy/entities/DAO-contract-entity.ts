713b74eae7f526eeaa456642f2538c7a44424707
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2024-05-15 17:11:37 +0600

    feat:fincube-103 swagger API setup and Auth0 configurations added

--- backend/web3-proxy-service/src/web3-dao-proxy/entities/DAO-contract-entity.ts
+++ backend/web3-proxy-service/src/web3-dao-proxy/entities/DAO-contract-entity.ts
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: '.env.local' });
-const artifacts = require('../../../../../web3/artifacts/contracts/FincubeDAO.sol/FinCubeDAO.json')
+const artifacts = require('../../../artifacts/contracts/FincubeDAO.sol/FinCubeDAO.json')
 export class DAOContract {
     address: any = process.env.CONTRACT_ADDRESS;
     abi: any = artifacts.abi;
--- backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.controller.ts
+++ backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { Web3ProxyService } from './web3-proxy.service';
-
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBody } from '@nestjs/swagger';
+import { RegisterMemberBody } from './entities/RegisterMemberInterface';
+import { Proposal } from './entities/ProposalInterface';
 
 @Controller('web3-dao-proxy')
 export class Web3ProxyController {
@@ -12,26 +15,33 @@ export class Web3ProxyController {
   }
 
   @Get('proposal-threshold')
+  @UseGuards(AuthGuard('jwt'))
   async getProposalThreshold(): Promise<number> {
     return this.web3ProxyService.getProposalThreshold();
   }
 
   @Get('proposal-count')
+  @UseGuards(AuthGuard('jwt'))
   async getOngoingProposalCount(): Promise<number> {
     return this.web3ProxyService.getOngoingProposalCount();
   }
 
   @Get('ongoing-proposals')
+  @UseGuards(AuthGuard('jwt'))
   async getOngoingProposals(): Promise<any> {
     return this.web3ProxyService.getOngoingProposals();
   }
 
   @Post('register-member')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBody({ type: RegisterMemberBody })
   async registerMember(@Body('address') address: string, @Body('_memberURI') _memberURI: string): Promise<any> {
     return this.web3ProxyService.registerMember(address, _memberURI);
   }
 
   @Post('execute-proposal')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBody({ type: Proposal })
   async executeProposal(@Body('proposalId') proposalId: number): Promise<any> {
     return this.web3ProxyService.executeProposal(proposalId);
   }
