8e814fca3e31d259a9ffce17e02f227786a7a7fb
Antonin Islam <antonin.islam@brainstation-23.com>
2024-05-16 14:58:06 +0600

    adds currency exchange uis

--- frontend/src/pages/tests.tsx
+++ frontend/src/pages/tests.tsx
@@ -1,10 +1,12 @@
-import CurrencyTranster from "@components/CurrencyTranster";
+import CurrencyExchange from "@components/currency/CurrencyExchange";
+import CurrencyTranster from "@components/currency/CurrencyTranster";
 
 export default function Tests() {
   return (
     <div className="container mx-auto mt-20">
       <div className="flex flex-col gap-5">
         <CurrencyTranster />
+        <CurrencyExchange />
       </div>
     </div>
   )
