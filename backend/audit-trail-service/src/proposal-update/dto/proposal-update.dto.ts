37c9064fb89f0f08042f45fbd2eca083e9f9a768
SampadSikder <sampadsikder84@gmail.com>
2024-06-21 13:06:07 +0600

    feat: fincube-128 created subclasses of dtos to handle proposal

--- backend/audit-trail-service/src/proposal-update/dto/proposal-update.dto.ts
+++ backend/audit-trail-service/src/proposal-update/dto/proposal-update.dto.ts
@@ -1,15 +1,27 @@
 import { WEB3STATUS, web3StatusCode } from 'src/shared/common/constants';
 import { ResponseTransactionStatusDto } from 'src/shared/common/dto/response-transaction-status.dto';
 
-export class ProposalUpdateDto {
+export class ProposalDto {
 
     public id: string;
     public proposalAddress: string;
-    proposer_address: string;
-    metadata: string;
-    transaction_info: ResponseTransactionStatusDto;
-    external_proposal: boolean;
+   
   
   }
+//A superclass will have the fields common to all DTOs. The child classes will deal with specifics
+class CreatedProposalDto extends ProposalDto{
+  proposer_address: string;
+  metadata: string;
+  transaction_info: ResponseTransactionStatusDto;
+  external_proposal: boolean;
+
+}
+
+
+class UpdatedProposalDto extends ProposalDto{
+  transaction_info: ResponseTransactionStatusDto;
+  
+}
+
   
   
