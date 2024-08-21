ac7dcc98c03e7295bbddb9a5ccdf8e1421a760f2
SampadSikder <sampadsikder84@gmail.com>
2024-08-21 17:29:17 +0600

    hotfix: fincube #18 logger fixes on DAO service

--- backend/dao-service/src/voting/voting.service.ts
+++ backend/dao-service/src/voting/voting.service.ts
@@ -1,27 +1,29 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { DAOEntity } from './entities/dao.entity';
 import axios from 'axios';
 @Injectable()
 export class VotingService {
+  private readonly logger = new Logger(VotingService.name);
   constructor(
-    @InjectRepository(DAOEntity) private daoRepository: Repository<DAOEntity>,
-  ) { }
+    @InjectRepository(DAOEntity) private daoRepository: Repository<DAOEntity>) 
+   { }
 
   private async getUserRole(sub: string): Promise<string> {
     try {
       const response = await axios.get(`http://user_management_api:3000/authentication/${sub}`);
       return response.data;
     } catch (error) {
+      this.logger.error("Role of user not found");
       throw new UnauthorizedException("Role of user not found");;
     }
   }
 
   async create(sub: string, dao: Partial<DAOEntity>): Promise<DAOEntity> {
     const role = await this.getUserRole(sub);
-    console.log(role);
     if (role != 'MFS') {
+      this.logger.error("User does not have permission "+ role);
       throw new UnauthorizedException("User does not have permission");
     }
     const new_dao = this.daoRepository.create(dao);
@@ -31,6 +33,7 @@ export class VotingService {
   async findOne(sub: string, id: number): Promise<DAOEntity> {
     const role = await this.getUserRole(sub);
     if (role != 'MFS') {
+      this.logger.error("User does not have permission "+ role);
       throw new UnauthorizedException("User does not have permission");
     }
     return this.daoRepository.findOne({ where: { id } });
@@ -42,6 +45,7 @@ export class VotingService {
   ): Promise<DAOEntity> {
     const role = await this.getUserRole(sub);
     if (role != 'MFS') {
+      this.logger.error("User does not have permission "+ role);
       throw new UnauthorizedException("User does not have permission");
     }
     await this.daoRepository.update(id, updated_dao);
