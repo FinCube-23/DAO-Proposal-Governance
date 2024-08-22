5ebcb4253ab9e6e07d070b5c3fb0b164308284db
Raad05 <ashabulraad@gmail.com>
2024-08-22 17:10:54 +0600

    feat: fincube #26 smart contract function calls

--- frontend/src/pages/dao_dashboard.tsx
+++ frontend/src/pages/dao_dashboard.tsx
@@ -1,116 +1,111 @@
 import ProposalCard from "@components/dao/ProposalCard";
 import { Button } from "@components/ui/button";
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardContent,
-    CardFooter,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
 } from "@components/ui/card";
 import { Proposal } from "@services/proposal/types";
 import { Box, Coins, Flag, Vote, WalletCards } from "lucide-react";
 
 const proposals: Proposal[] = [
-    {
-        title: "Renovation Project",
-        description: "Renovating the community center",
-        status: "ongoing",
-        address: "0x1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0",
-    },
-    {
-        title: "Education Program",
-        description: "Funding educational workshops",
-        status: "executed",
-        address: "0x3e4f5a6b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2",
-    },
-    {
-        title: "Park Cleanup",
-        description: "Organizing a community park cleanup event",
-        status: "canceled",
-        address: "0x5a6b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2f3a4",
-    },
+  {
+    title: "Renovation Project",
+    description: "Renovating the community center",
+    status: "ongoing",
+    address: "0x1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0",
+  },
+  {
+    title: "Education Program",
+    description: "Funding educational workshops",
+    status: "executed",
+    address: "0x3e4f5a6b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2",
+  },
+  {
+    title: "Park Cleanup",
+    description: "Organizing a community park cleanup event",
+    status: "canceled",
+    address: "0x5a6b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2f3a4",
+  },
 ];
 
 export default function DaoDashboard() {
-    return (
-        <div className="flex flex-col gap-5">
-            <Card>
-                <CardHeader>
-                    <CardTitle className="text-2xl">DAO Title Dashboard</CardTitle>
-                    <CardDescription>
-                        <a
-                            href="#"
-                            className="text-sm text-green-500 cursor-pointer hover:underline"
-                        >
-                            
-                        </a>
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Fugiat, corrupti fuga! Laudantium vero libero ipsa
-                        possimus quos a provident sunt cupiditate neque, ipsam
-                        totam facere temporibus nihil. Sapiente, praesentium
-                        magni!
-                    </p>
-                </CardContent>
-                <CardFooter>
-                    <div className="flex gap-3 text-sm">
-                        <div className="flex gap-1">
-                            <Flag className="text-green-500" /> December 2023
-                        </div>
-                        <div className="flex gap-1">
-                            <Box className="text-green-500" />
-                            Polygon
-                        </div>
-                        <div className="flex gap-1">
-                            <WalletCards className="text-green-500" />
-                            Wallet-based
-                        </div>
-                    </div>
-                </CardFooter>
-            </Card>
-            <div className="flex flex-col-reverse md:grid md:grid-cols-12 gap-5">
-                <div className="md:col-span-7 flex flex-col gap-5">
-                    <Card>
-                        <div className="flex justify-between items-center p-3">
-                            <div className="flex items-center gap-3">
-                                <Vote className="text-green-500" />
-                                <p>12 Proposal(s) created</p>{" "}
-                            </div>
-                            <div>
-                                <Button>New Proposal</Button>
-                            </div>
-                        </div>
-                    </Card>
-                    {/* Proposal List */}
-                    {proposals.map((proposal: Proposal, idx: number) => (
-                        <ProposalCard key={idx} proposal={proposal} />
-                    ))}
-                </div>
-                <div className="md:col-span-5">
-                    <Card>
-                        <CardHeader>
-                            <div className="flex justify-between items-center">
-                                <div className="bg-green-100 p-2 rounded-lg">
-                                    <Coins className="text-green-500" />
-                                </div>
-                                <div>
-                                    <Button>New Member</Button>
-                                </div>
-                            </div>
-                        </CardHeader>
-                        <CardContent>
-                            <div className="font-bold text-2xl">$10,000</div>
-                            <div className="text-muted-foreground">
-                                Treasury value
-                            </div>
-                        </CardContent>
-                    </Card>
-                </div>
+  return (
+    <div className="flex flex-col gap-5">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">DAO Title Dashboard</CardTitle>
+          <CardDescription>
+            <a
+              href="#"
+              className="text-sm text-green-500 cursor-pointer hover:underline"
+            ></a>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat,
+            corrupti fuga! Laudantium vero libero ipsa possimus quos a provident
+            sunt cupiditate neque, ipsam totam facere temporibus nihil.
+            Sapiente, praesentium magni!
+          </p>
+        </CardContent>
+        <CardFooter>
+          <div className="flex gap-3 text-sm">
+            <div className="flex gap-1">
+              <Flag className="text-green-500" /> December 2023
             </div>
+            <div className="flex gap-1">
+              <Box className="text-green-500" />
+              Polygon
+            </div>
+            <div className="flex gap-1">
+              <WalletCards className="text-green-500" />
+              Wallet-based
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+      <div className="flex flex-col-reverse md:grid md:grid-cols-12 gap-5">
+        <div className="md:col-span-7 flex flex-col gap-5">
+          <Card>
+            <div className="flex justify-between items-center p-3">
+              <div className="flex items-center gap-3">
+                <Vote className="text-green-500" />
+                <p>12 Proposal(s) created</p>{" "}
+              </div>
+              <div>
+                <Button>New Proposal</Button>
+              </div>
+            </div>
+          </Card>
+          {/* Proposal List */}
+          {proposals.map((proposal: Proposal, idx: number) => (
+            <ProposalCard key={idx} proposal={proposal} />
+          ))}
         </div>
-    );
+        <div className="md:col-span-5">
+          <Card>
+            <CardHeader>
+              <div className="flex justify-between items-center">
+                <div className="bg-green-100 p-2 rounded-lg">
+                  <Coins className="text-green-500" />
+                </div>
+                <div>
+                  <Button>New Member</Button>
+                </div>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div className="font-bold text-2xl">$10,000</div>
+              <div className="text-muted-foreground">Treasury value</div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/welcome.tsx
+++ frontend/src/pages/welcome.tsx
@@ -1,8 +1,291 @@
 import { Button } from "@components/ui/button";
 import { SparklesCore } from "@components/ui/sparkles";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  readContract,
+  simulateContract,
+  waitForTransactionReceipt,
+  writeContract,
+} from "@wagmi/core";
+import { config } from "@layouts/RootLayout";
 
 export default function welcome() {
+  // getter function states
+  const [proposalCount, setProposalCount] = useState(0);
+  const [proposals, setProposals] = useState([]);
+  const [proposalsByPage, setProposalsByPage] = useState({
+    paginateProposals: [],
+    newCursor: 0,
+  });
+  const [proposalThreshold, setProposalThreshhold] = useState(0);
+  const [delay, setDelay] = useState(0);
+  const [period, setPeriod] = useState(0);
+
+  // SETTER FUNCTIONS
+
+  // registerMember()
+  const register = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "registerMember",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Registration successful");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // newMemberApprovalProposal()
+  const approveMember = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "newMemberApprovalProposal",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Member approved!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // propose()
+  const propose = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "propose",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Members proposed!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // castVote()
+  const castVote = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "castVote",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Vote casted!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // executeProposal()
+  const executeProposal = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "executeProposal",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Proposal executed!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // cancelProposal()
+  const cancelProposal = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "cancelProposal",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Proposal cancelled!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // setVotingDelay()
+  const setDelay = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "setVotingDelay",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Delay set!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // setPeriod()
+  const setPeriod = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+      const { request } = await simulateContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "setVotingPeriod",
+        args: [""], // pass arguments
+      });
+
+      const hash = await writeContract(config, request);
+
+      await waitForTransactionReceipt(config, { hash });
+
+      alert("Period set!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // GETTER FUNCTIONS
+
+  // getOngoingProposalsCount()
+  const getProposalCount = async () => {
+    try {
+      const response = await readContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "getOngoingProposalsCount",
+      });
+
+      setProposalCount(response as number);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // getOngoingProposals()
+  const getProposals = async () => {
+    try {
+      const response = await readContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "getOngoingProposals",
+      });
+
+      setProposals(response as []);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // getProposalsByPage()
+  const getProposalsByPage = async () => {
+    try {
+      const response = await readContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "getProposalsByPage",
+      });
+
+      setProposalsByPage(
+        response as { paginateProposals: []; newCursor: number }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // proposalThreshold()
+  const getThreshhold = async () => {
+    try {
+      const response = await readContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "proposalThreshold",
+      });
+
+      setProposalThreshhold(response as number);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // getVotingDelay()
+  const getDelay = async () => {
+    try {
+      const response = await readContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "getVotingDelay",
+      });
+
+      setDelay(response as number);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // getVotingPeriod()
+  const getPeriod = async () => {
+    try {
+      const response = await readContract(config, {
+        abi: [], // Fill
+        address: "0x", // Fill
+        functionName: "getVotingPeriod",
+      });
+
+      setPeriod(response as number);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
       <div className="h-screen relative w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
@@ -22,9 +305,11 @@ export default function welcome() {
             FinCube 23
           </h1>
           {/* <div className="z-40"><Link to={"/dashboard"}><Button>Go to Demo Dashboard</Button></Link></div> */}
-          <h2 className="text-xl text-center text-white">Secure. Swift. Seamless</h2>
+          <h2 className="text-xl text-center text-white">
+            Secure. Swift. Seamless
+          </h2>
         </div>
       </div>
     </>
-  )
+  );
 }
