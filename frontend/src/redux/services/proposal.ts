39cdb236e8b656459e5b8abb726389b07b0e74e1
Luci4 <yamin.raad6109@gmail.com>
2024-11-11 15:38:30 +0600

    feat: fincube #143 integrated Web3Proxy Service and DAO Service

---
+++ frontend/src/redux/services/proposal.ts
@@ -0,0 +1,37 @@
+import { api } from "@redux/api/base";
+import { PROPOSAL_ENDPOINT } from "@redux/api/endpoints";
+import {
+  CreateProposalPayload,
+  CreateProposalResponse,
+  GetProposalResponse,
+} from "@redux/api/types";
+
+export const proposalApis = api.injectEndpoints({
+  endpoints: (build) => ({
+    // get proposal
+    getProposal: build.query<GetProposalResponse, void>({
+      query: () => {
+        return {
+          url: PROPOSAL_ENDPOINT.BASE,
+          method: "GET",
+        };
+      },
+      providesTags: ["proposal"],
+    }),
+    // create proposal
+    createProposal: build.mutation<
+      CreateProposalResponse,
+      CreateProposalPayload
+    >({
+      query: (payload) => ({
+        url: PROPOSAL_ENDPOINT.BASE,
+        method: "POST",
+        body: payload,
+      }),
+      invalidatesTags: ["proposal"],
+    }),
+  }),
+});
+
+export const { useLazyGetProposalQuery, useCreateProposalMutation } =
+  proposalApis;
