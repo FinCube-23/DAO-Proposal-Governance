03f7e13003ec30e7ca6c8cbdde485ad38653699b
Luci4 <yamin.raad6109@gmail.com>
2025-01-08 13:49:23 +0600

    fix: resolved super admin dashboard endpoint

--- frontend/src/routes/admin.tsx
+++ frontend/src/routes/admin.tsx
@@ -1,14 +1,8 @@
 import AdminLayout from "@layouts/AdminLayout";
-import Dashboard from "@pages/admin/Dashboard";
 import { RouteObject } from "react-router-dom";
 
 export const adminRoutes: RouteObject = {
   path: "/admin",
   element: <AdminLayout></AdminLayout>,
-  children: [
-    {
-      path: "",
-      element: <Dashboard></Dashboard>,
-    },
-  ],
+  children: [],
 };
