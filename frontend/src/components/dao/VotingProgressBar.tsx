b1eefc5f197743c7d5ab86d4fe6d50d4c0899bfb
Luci4 <yamin.raad6109@gmail.com>
2025-02-10 19:40:01 +0600

    feat: fincube #210 added vote labels section

--- frontend/src/components/dao/VotingProgressBar.tsx
+++ frontend/src/components/dao/VotingProgressBar.tsx
@@ -1,27 +1,27 @@
 interface Props {
-    total: number;
-    yes: number;
-    no: number;
+  total: number;
+  yes: number;
+  no: number;
 }
 export default function VotingProgressBar({ total, yes, no }: Props) {
-    const neutral = total - (yes + no);
-    const yesPercent = (yes / total) * 100;
-    const noPercent = (no / total) * 100;
-    const neutralPercent = (neutral / total) * 100;
-    return (
-        <div className="border h-4 my-2 text-xs flex rounded-lg bg-emerald-200">
-            <div
-                style={{ width: `${yesPercent}%` }}
-                className="shadow-none flex flex-col text-center whitespace-nowrap rounded-l-lg text-white justify-center bg-green-400"
-            >Yes</div>
-            <div
-                style={{ width: `${noPercent}%` }}
-                className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-400"
-            >No</div>
-            <div
-                style={{ width: `${neutralPercent}%` }}
-                className="shadow-none flex flex-col text-center rounded-r-lg whitespace-nowrap text-white justify-center bg-gray-300"
-            >None</div>
-        </div>
-    );
+  const neutral = total - (yes + no);
+  const yesPercent = (yes / total) * 100;
+  const noPercent = (no / total) * 100;
+  const neutralPercent = (neutral / total) * 100;
+  return (
+    <div className="border h-4 my-2 text-xs flex rounded-lg bg-emerald-200">
+      <div
+        style={{ width: `${yesPercent}%` }}
+        className="shadow-none flex flex-col text-center whitespace-nowrap rounded-l-lg text-white justify-center bg-green-400"
+      ></div>
+      <div
+        style={{ width: `${noPercent}%` }}
+        className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-400"
+      ></div>
+      <div
+        style={{ width: `${neutralPercent}%` }}
+        className="shadow-none flex flex-col text-center rounded-r-lg whitespace-nowrap text-black justify-center bg-gray-300"
+      ></div>
+    </div>
+  );
 }
