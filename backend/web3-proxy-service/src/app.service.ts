1ef57f4b394d60a3070189a43c77d5fb20c96591
fahimdev <fahim.arif0373@outlook.com>
2025-02-11 18:24:28 +0600

    ref: #206 Updates web3 proxy service realtime updatable

--- backend/web3-proxy-service/src/app.service.ts
+++ backend/web3-proxy-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to WEB3-PROXY-SERVICE node🚀';
+    return 'Welcome to WEB3-PROXY-SERVICE node! 🚀';
   }
 }
