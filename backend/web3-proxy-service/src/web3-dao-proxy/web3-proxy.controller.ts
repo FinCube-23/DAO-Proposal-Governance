c78b8cf10cb304dc417ec617a4fa39f6624d6044
SampadSikder <bsse1219@iit.du.ac.bd>
2025-02-18 15:46:31 +0600

    feat: fincube #235 Authguard removal and sepolia added for web3-proxy

--- backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.controller.ts
+++ backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.controller.ts
@@ -10,51 +10,43 @@ export class Web3ProxyController {
   constructor(private readonly web3ProxyService: Web3ProxyService) { }
 
   @Post('balance')
-  @UseGuards(AuthGuard('jwt'))
   async getBalance(@Req() req, @Body('address') address: string) {
     return this.web3ProxyService.getBalance(address, req.user);
   }
 
   @Get('proposal-threshold')
-  @UseGuards(AuthGuard('jwt'))
   async getProposalThreshold(@Req() req): Promise<number> {
     return this.web3ProxyService.getProposalThreshold(req.user);
   }
 
   @Get('proposal-count')
-  @UseGuards(AuthGuard('jwt'))
   async getOngoingProposalCount(@Req() req): Promise<number> {
     return this.web3ProxyService.getOngoingProposalCount(req.user);
   }
 
   @Get('ongoing-proposals')
-  @UseGuards(AuthGuard('jwt'))
   async getOngoingProposals(@Req() req): Promise<any> {
     return this.web3ProxyService.getOngoingProposals(req.user);
   }
 
   @Post('proposals-by-page')
-  @UseGuards(AuthGuard('jwt'))
   async getProposalsByPage(@Req() req, @Body('cursor') cursor: number, @Body('howMany') howMany: number): Promise<any> {
     return this.web3ProxyService.getProposalsByPage(cursor, howMany, req.user);
   }
 
   @Post('register-member')
-  @UseGuards(AuthGuard('jwt'))
   @ApiBody({ type: RegisterMemberBody })
   async registerMember(@Req() req, @Body('address') address: string, @Body('_memberURI') _memberURI: string): Promise<any> {
     return this.web3ProxyService.registerMember(address, _memberURI, req.user);
   }
 
   @Post('execute-proposal')
-  @UseGuards(AuthGuard('jwt'))
   @ApiBody({ type: Proposal })
   async executeProposal(@Req() req, @Body('proposalId') proposalId: number): Promise<any> {
     return this.web3ProxyService.executeProposal(proposalId, req.user);
   }
 
   @Post('is-member-approved')
-  @UseGuards(AuthGuard('jwt'))
   @ApiBody({ type: Proposal })
   async checkIsMemberApproved(@Req() req, @Body('address') memberAddress: string): Promise<any> {
     return this.web3ProxyService.checkIsMemberApproved(memberAddress, req.user);
--- backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.service.ts
+++ backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.service.ts
@@ -12,88 +12,56 @@ export class Web3ProxyService {
     const wallet = new ethers.Wallet(this.daoContract.signer, provider);
     this.contract = new ethers.Contract(this.daoContract.address, this.daoContract.abi, wallet);
   }
-  private async getUserRole(sub: string): Promise<string> {
-    try {
-      const response = await axios.get(`http://user_management_api:3000/authentication/${sub}`);
-      return response.data;
-    } catch (error) {
-      this.logger.error("Error getting user role");
-      throw new UnauthorizedException("Role of user not found");;
-    }
-  }
+
 
   private provider(): ethers.JsonRpcProvider {
     const provider = new ethers.JsonRpcProvider(this.JSONRPCProvider.ALCHEMY_ENDPOINT);
     return provider;
   }
   async getBalance(address: string, sub: string): Promise<number> {
-    const role = await this.getUserRole(sub);
-    if (role != 'MFS') {
-      throw new UnauthorizedException("User does not have permission");
-    }
     const provider = this.provider();
     const balance = await provider.getBalance(address);
     return Number(balance);
   }
 
   async getProposalThreshold(sub: string): Promise<number> {
-    const role = await this.getUserRole(sub);
-    if (role != 'MFS') {
-      this.logger.error("Error getting user role");
-      throw new UnauthorizedException("User does not have permission");
-    }
     return await this.contract.proposalThreshold();
   }
 
   async getOngoingProposalCount(sub: string): Promise<number> {
-    const role = await this.getUserRole(sub);
-    if (role != 'MFS') {
-      this.logger.error("Error getting user role");
-      throw new UnauthorizedException("User does not have permission");
-    }
     return await this.contract.getOngoingProposalsCount();
   }
 
   async getOngoingProposals(sub: string): Promise<any> {
-    const role = await this.getUserRole(sub);;
-    if (role != 'MFS') {
-      this.logger.error("Error getting user role");
-      throw new UnauthorizedException("User does not have permission");
-    }
     return await this.contract.getOngoingProposals();
   }
 
   async registerMember(address: string, _memberURI: string, sub: string): Promise<any> {
-    const role = await this.getUserRole(sub);
-    if (role != 'MFS') {
-      this.logger.error("Error getting user role");
-      throw new UnauthorizedException("User does not have permission");
-    }
     return await this.contract.registerMember(address, _memberURI);
   }
 
   async executeProposal(proposalId: number, sub: string): Promise<any> {
-    const role = await this.getUserRole(sub);
-    if (role != 'MFS') {
-      this.logger.error("Error getting user role");
-      throw new UnauthorizedException("User does not have permission");
-    }
     return await this.contract.executeProposal(proposalId);
   }
 
-  async checkIsMemberApproved(memberAddress: string, sub:string): Promise<any> {
-    const role = await this.getUserRole(sub);
-    if (role!= 'MFS') {
-      throw new UnauthorizedException("User does not have permission");
-    }
+  async checkIsMemberApproved(memberAddress: string, sub: string): Promise<any> {
     return await this.contract.checkIsMemberApproved(memberAddress);
   }
 
-   async getProposalsByPage(cursor: number, howMany: number, sub:string): Promise<any>{
-    const role = await this.getUserRole(sub);
-    if (role!= 'MFS') {
-      throw new UnauthorizedException("User does not have permission");
-    }
-    return await this.contract.getProposalsByPage(cursor, howMany);
-   }
+
+  parseBigInt(array: any[]) {
+    return JSON.parse(JSON.stringify(array, (key, value) =>
+      typeof value === 'bigint'
+        ? value.toString()
+        : value // return everything else unchanged
+    ));
+  }
+
+  async getProposalsByPage(cursor: number, howMany: number, sub: string): Promise<any> {
+    const proposals = await this.contract.getProposalsByPage(cursor, howMany);
+
+    const formattedProposals = this.parseBigInt(proposals);
+
+    return formattedProposals;
+  }
 }
