7b87a8c6aacf60037d5b2c0814000c3452db596c
Antonin Islam <ant@mac.local>
2025-03-25 15:30:35 +0600

    feat: fincube #260 adds auth guard to dao-service and web3-proxy-service

--- backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.module.ts
+++ backend/web3-proxy-service/src/web3-dao-proxy/web3-proxy.module.ts
@@ -3,9 +3,22 @@ import { Web3ProxyService } from './web3-proxy.service';
 import { Web3ProxyController } from './web3-proxy.controller';
 import { DAOContract } from './entities/DAO-contract-entity';
 import { RPCProvider } from './entities/RPC-Provider-entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_MANAGEMENT_SERVICE', // Injectable
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://rabbitmq:5672'],
+          queue: 'authorization', // Routing Key
+        },
+      },
+    ]),
+  ],
   controllers: [Web3ProxyController],
   providers: [Web3ProxyService, DAOContract, RPCProvider],
 })
-export class Web3ProxyModule { }
+export class Web3ProxyModule {}
