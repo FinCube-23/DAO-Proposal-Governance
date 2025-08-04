0982fc39b20906847bb790aa275c6f3a24312397
FahimDev <ariful.islam@brainstation-23.com>
2025-08-04 15:41:56 +0600

    task: #300 Removes NestJS UMS

--- backend/user-management-service/src/database/database.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { DatabaseService } from './database.service';
-
-@Controller()
-export class DatabaseController {
-  constructor(private readonly databaseService: DatabaseService) {}
-}
--- backend/user-management-service/src/mfs_business/dtos/response-transaction-status.dto.ts
+++
@@ -1,7 +0,0 @@
-export class ResponseTransactionStatusDto {
-    web3Status: number;
-    message: string;
-    data?: Record<string, any>;
-    blockNumber: number;
-    transactionHash: string;
-}
