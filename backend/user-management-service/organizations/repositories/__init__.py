533f2d622bf3a58baf03a739db8885d6f0d0d413
Samonto Karmaker <samonto0726@gmail.com>
2025-08-12 16:53:00 +0600

    feat: fincube #308 Add OrganizationUserRepository to the exports in the repository module

--- backend/user-management-service/organizations/repositories/__init__.py
+++ backend/user-management-service/organizations/repositories/__init__.py
@@ -1,3 +1,4 @@
 from .organization_repository import OrganizationRepository
+from .orgnaization_user_repository import OrganizationUserRepository
 
-__all__ = ['OrganizationRepository']
+__all__ = ['OrganizationRepository', 'OrganizationUserRepository']
