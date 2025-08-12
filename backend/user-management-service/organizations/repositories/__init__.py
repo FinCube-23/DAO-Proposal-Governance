3cde428561d8b9522bd7640ff0c85e52c611cecd
Samonto Karmaker <samonto0726@gmail.com>
2025-08-12 17:09:47 +0600

    fix: fincube #308 Fix typo in import statement for OrganizationUserRepository

--- backend/user-management-service/organizations/repositories/__init__.py
+++ backend/user-management-service/organizations/repositories/__init__.py
@@ -1,4 +1,4 @@
 from .organization_repository import OrganizationRepository
-from .orgnaization_user_repository import OrganizationUserRepository
+from .organization_user_repository import OrganizationUserRepository
 
 __all__ = ['OrganizationRepository', 'OrganizationUserRepository']
