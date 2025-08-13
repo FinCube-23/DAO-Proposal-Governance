dcbc56f5e7959467fa9ce91157f899aa967a8872
Samonto Karmaker <samonto0726@gmail.com>
2025-08-13 15:51:35 +0600

    refactor:  fincube #308 Update URL pattern for onchain verifications

--- backend/user-management-service/organizations/urls.py
+++ backend/user-management-service/organizations/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('onchain-verifications/', 
          OnchainVerificationController.as_view({'post': 'create'}), 
          name='onchain-verifications-create'),
-    path('onchain-verifications/by-organization/<int:org_id>/', 
+    path('<int:org_id>/onchain-verifications/', 
          OnchainVerificationController.as_view({'get': 'get_onchain_verifications_by_organization'}), 
          name='onchain-verifications-by-org'),
 ]
