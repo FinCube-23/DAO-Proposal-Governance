4dc0934786789f3ff0d48393974c2dc69385a466
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-08-13 15:15:36 +0600

    feat: fincube #309 Added JWT Validation for Password Controller

--- backend/user-management-service/users/controllers/auth_controller.py
+++ backend/user-management-service/users/controllers/auth_controller.py
@@ -1,15 +1,20 @@
 from rest_framework.views import APIView
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample, OpenApiTypes, OpenApiParameter
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiParameter,
+    OpenApiExample,
+    OpenApiTypes,
+    OpenApiParameter,
+)
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from django.contrib.auth import authenticate
 
 
-from users.serializers import (
-    PasswordUpdateSerializer,
-    UserLoginSerializer
-)
+from users.serializers import PasswordUpdateSerializer, UserLoginSerializer
 from users.services import UserService
 from users.utils import get_tokens_for_user
 
@@ -19,8 +24,8 @@ class AuthController(APIView):
         request=UserLoginSerializer,
         responses={
             200: {"type": "object", "properties": {"status": {"type": "string"}}},
-            400: {"type": "object", "properties": {"error": {"type": "string"}}}
-        }
+            400: {"type": "object", "properties": {"error": {"type": "string"}}},
+        },
     )
     def post(self, request):
         login_serializer = UserLoginSerializer(data=request.data)
@@ -29,8 +34,8 @@ class AuthController(APIView):
         try:
             # Authenticate user
             user = authenticate(
-                email=login_serializer.validated_data['email'],
-                password=login_serializer.validated_data['password']
+                email=login_serializer.validated_data["email"],
+                password=login_serializer.validated_data["password"],
             )
             if not user:
                 raise ValueError("Invalid credentials")
@@ -41,42 +46,46 @@ class AuthController(APIView):
             # Generate tokens
             tokens = get_tokens_for_user(user)
 
-            return Response({
-                'status': 'success',
-                'tokens': tokens
-            }, status=status.HTTP_200_OK)
+            return Response(
+                {"status": "success", "tokens": tokens}, status=status.HTTP_200_OK
+            )
 
         except ValueError as e:
-            return Response({
-                'status': 'error',
-                'message': str(e)
-            }, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"status": "error", "message": str(e)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         except Exception as e:
-            return Response({
-                'status': 'error',
-                'message': f"An unexpected error occurred: {str(e)}"
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+            return Response(
+                {
+                    "status": "error",
+                    "message": f"An unexpected error occurred: {str(e)}",
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
 
 class PasswordController(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+
     @extend_schema(
         request=PasswordUpdateSerializer,
         responses={
             200: {"type": "object", "properties": {"status": {"type": "string"}}},
-            400: {"type": "object", "properties": {"error": {"type": "string"}}}
-        }
+            400: {"type": "object", "properties": {"error": {"type": "string"}}},
+        },
     )
     def post(self, request, user_id):
         serializer = PasswordUpdateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
+
         try:
             user = UserService.update_password(
                 user_id,
-                serializer.validated_data['current_password'],
-                serializer.validated_data['new_password']
+                serializer.validated_data["current_password"],
+                serializer.validated_data["new_password"],
             )
             return Response({"status": "Password updated successfully"})
         except Exception as e:
