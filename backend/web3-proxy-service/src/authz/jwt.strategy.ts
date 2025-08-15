fe004bc3f6c5fcd893c9f72e1f302fa009f2647e
Luci4 <yamin.raad6109@gmail.com>
2025-08-15 20:44:33 +0600

    refactor: #313 docker optimization for web3-proxy-service

--- backend/web3-proxy-service/src/authz/jwt.strategy.ts
+++ backend/web3-proxy-service/src/authz/jwt.strategy.ts
@@ -3,27 +3,27 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { passportJwtSecret } from 'jwks-rsa';
 import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env' });
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
-        super({
-            secretOrKeyProvider: passportJwtSecret({
-                cache: true,
-                rateLimit: true,
-                jwksRequestsPerMinute: 5,
-                jwksUri: `${process.env.AUTH0_ISSUER_URL}.well-known/jwks.json`,
-            }),
+  constructor() {
+    super({
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${process.env.AUTH0_ISSUER_URL}.well-known/jwks.json`,
+      }),
 
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            audience: process.env.AUTH0_AUDIENCE,
-            issuer: `${process.env.AUTH0_ISSUER_URL}`,
-            algorithms: ['RS256'],
-        });
-    }
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: `${process.env.AUTH0_ISSUER_URL}`,
+      algorithms: ['RS256'],
+    });
+  }
 
-    validate(payload: any): string {
-        return payload.sub;
-    }
+  validate(payload: any): string {
+    return payload.sub;
+  }
 }
