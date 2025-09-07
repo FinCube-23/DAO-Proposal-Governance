0ac294777b652f8e4d5e57104a1d3ea81f58ee2d
Antonin Islam <antonin686.dev@gmail.com>
2025-09-08 05:52:52 +0600

    refactor: fincube #334 shadcn ui components

--- frontend/app/shared/components/ui/skeleton.tsx
+++ frontend/app/shared/components/ui/skeleton.tsx
@@ -1,13 +1,13 @@
-import { cn } from "@/shared/utils/index"
+import { cn } from '@/shared/utils/index';
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      className={cn('bg-accent animate-pulse rounded-md', className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
