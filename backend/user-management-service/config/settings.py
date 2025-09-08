7379ce44e1908f46259c068e0ff21f01628507e4
Raad05 <yamin.raad6109@gmail.com>
2025-09-08 13:09:23 +0600

    hotfix: updated allowed hosts

--- backend/user-management-service/config/settings.py
+++ backend/user-management-service/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
 # Application definition
