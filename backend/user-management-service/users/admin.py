6b6208634f4bb642ba3db832961a3b484e59b24e
Samonto Karmaker <samonto0726@gmail.com>
2025-09-23 14:03:49 +0600

    feat: fincube #323 reorder list_display fields for admin panel to change clickable element to email

--- backend/user-management-service/users/admin.py
+++ backend/user-management-service/users/admin.py
@@ -6,8 +6,8 @@ from .models import User
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = [
-        'id',
         'email',
+        'id',
         'first_name', 
         'last_name',
         'status',
