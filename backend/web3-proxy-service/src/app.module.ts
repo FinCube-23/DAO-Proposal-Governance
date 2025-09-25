543041367c85058accece71ce967c74ad4c5cded
omitul.islam <omitul.islam@brainstation-23.com>
2025-09-25 16:34:40 +0600

    feat: web3-kit #344 updated module,main and dependencies, web3Service

--- backend/web3-proxy-service/src/app.module.ts
+++ backend/web3-proxy-service/src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, NestModule, Module} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Web3ProxyModule } from './web3-dao-proxy/web3-proxy.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthzModule } from './authz/authz.module';
+import {WinstonLogger} from './shared/common/logger/winston-logger';
+import {MorganMiddleware} from './shared/common/logger/morgan.middleware';
 @Module({
   imports: [
     Web3ProxyModule,
@@ -14,6 +16,12 @@ import { AuthzModule } from './authz/authz.module';
     AuthzModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, winstonLogger, MorganMiddleware],
+  exports: [winstonLogger]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // Apply MorganMiddleware globally
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MorganMiddleware).forRoutes('*');
+  }
+}
